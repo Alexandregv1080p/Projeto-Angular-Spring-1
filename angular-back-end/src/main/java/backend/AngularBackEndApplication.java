629db87de0704b7d0245f8405b1baefb31ae9eb9
Alexandregv1080p <alexandregv1999@gmail.com>
2023-07-27 09:01:21 -0300

    commit

---
+++ angular-back-end/src/main/java/backend/AngularBackEndApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularBackEndApplication.class, args);
+	}
+
+}

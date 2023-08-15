ca5e8c61c51d20dcf6246696fa82706f6488332d
Alexandregv1080p <alexandregv1999@gmail.com>
2023-08-15 07:39:03 -0300

    commit

--- backend/src/main/java/com/alexandre/backend/repository/UserRepository.java
+++ backend/src/main/java/com/alexandre/backend/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.alexandre.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alexandre.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
     
+    Optional<User> findByEmail(String email);
 }
---
+++ backend/src/main/java/com/alexandre/backend/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.alexandre.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractToUsername(String token) {
+        return null;
+    }
+
+}

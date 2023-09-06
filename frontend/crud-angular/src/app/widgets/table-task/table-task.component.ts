228d5ecc3352de7c68ebe94627e1a137e10ec323
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-06 14:33:35 -0300

    Arrumei

--- frontend/crud-angular/src/app/widgets/table-task/table-task.component.ts
+++ frontend/crud-angular/src/app/widgets/table-task/table-task.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Task } from 'src/app/model/Task';
+import { TasksService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-table-task',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-task.component.scss']
 })
 export class TableTaskComponent {
+  tasks: Observable<Task[]>
+  task!:Task[]
+  constructor(private taskService: TasksService,private router:Router){
+    this.tasks = this.taskService.list();
+  }
 
+  displayedColumns = ['id','cliente','nomeTarefa','status','actions']
+
+  ngOnInit(): void {
+      this.taskService.read().subscribe(task =>{
+        this.task = task
+      })
+  }
 }

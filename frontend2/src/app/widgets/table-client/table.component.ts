c144ef3d947ecb7b626610934345878221ec3cba
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-13 19:38:59 -0300

    commit

--- frontend2/src/app/widgets/table-client/table.component.ts
+++ frontend2/src/app/widgets/table-client/table.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Client } from 'src/app/model/Client';
 import { ClientServiceService } from 'src/app/services/client-service.service';
-import * as _ from 'lodash';
 import { Router } from '@angular/router';
 
 @Component({

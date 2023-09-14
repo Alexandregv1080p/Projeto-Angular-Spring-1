0894fdec940b8861d6445988cbff1b6885acfbe2
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-14 12:10:18 -0300

    commit

--- frontend/crud-angular/src/app/components/tasks/del-task/del-task.component.ts
+++ frontend/crud-angular/src/app/components/tasks/del-task/del-task.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Client } from 'src/app/model/Client';
 import { Task } from 'src/app/model/Task';
@@ -9,15 +9,15 @@ import { TasksService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-del-task',
   templateUrl: './del-task.component.html',
-  styleUrls: ['./del-task.component.scss']
+  styleUrls: ['./del-task.component.scss'],
 })
 export class DelTaskComponent implements OnInit {
-  form: FormGroup
-  clientes!:Client[]
-  task!:Task
+  form: FormGroup;
+  clientes!: Client[];
+  task!: Task;
 
   constructor(
-    private route:ActivatedRoute,
+    private route: ActivatedRoute,
     private router: Router,
     private taskService: TasksService,
     private formBuilder: FormBuilder,
@@ -26,22 +26,34 @@ export class DelTaskComponent implements OnInit {
     this.form = this.formBuilder.group({
       clientId: [null, [Validators.required]],
       nomeTarefa: ['', Validators.required],
-      status: [null, Validators.required]
+      status: [null, Validators.required],
     });
   }
 
   ngOnInit(): void {
-    let id = this.route.snapshot.params['id'];
-    this.taskService.readById(id).subscribe(task=>{
-      this.task = task
-    })
+
+    this.route.params.subscribe((params) => {
+      let id = this.route.snapshot.params['id'];
+      this.taskService.readById(id).subscribe((task) => {
+        this.task = task;
+        console.log(task)
+        this.form.patchValue({
+          clientId: this.task.cliente, // Use a propriedade correta aqui
+          nomeTarefa: this.task.nomeTarefa,
+          status: this.task.status,
+        });
+      });
+    });
   }
-  deleteTask(){
+
+  deleteTask() {
     this.taskService.remove(this.task.id).subscribe(() => {
-      this.taskService.showMensage("Tarefa deletada com sucesso!")
-      this.router.navigate(["clientes"])
-    })
+      this.taskService.showMensage('Tarefa deletada com sucesso!');
+      this.router.navigate(['clientes']);
+    });
   }
+
   onCancel() {
+    this.router.navigate(['clientes']);
   }
 }

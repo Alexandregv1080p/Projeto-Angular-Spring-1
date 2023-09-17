42ae6376765fc7072fabac699bb19969f20ef423
Alexandregv1080p <alexandregv1999@gmail.com>
2023-09-17 17:57:53 -0300

    commit

--- backend/src/main/java/com/alexandre/backend/BackendApplication.java
+++ backend/src/main/java/com/alexandre/backend/BackendApplication.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
